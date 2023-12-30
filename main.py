1b729c84b093301986af6d4e874642ec75ac5475
bcwlkr <bcwlkr@gmail.com>
2023-12-30 13:12:59 -0600

    Finished

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import pyautogui
-from pyautogui import press, typewrite, hotkey
+from pynput import keyboard
+from pynput.keyboard import Controller
 
 list = open("list.txt","r")
 data = list.read()
-data_into_list = data.replace('\n', ' ').split(".") 
+data_into_list = data.replace('\n', ' ').split(".") #cleans up the data and puts it into a list
 
 print(data_into_list)
 
 count = len(data_into_list)
 
+keyboard = Controller()
+I = 0
 for item in data_into_list:
-    I = 0
-    typewrite(I in data_into_list)  
+    keyboard.type(item) 
+    keyboard.press(keyboard.enter) 
     I = I + 1
     if I == 10:
         break
 
 
 list.close()
-    # Add an indented block here
-    #throws error without indent so i indent
